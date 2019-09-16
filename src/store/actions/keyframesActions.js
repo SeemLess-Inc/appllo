a84ea0a788d504db1a1f80525c4b82ef60d00ee3
John Dalziel <john@crashposition.com>
2019-09-16 19:32:50 +0100

    Move data into local JSON files.

--- src/store/actions/keyframesActions.js
+++ src/store/actions/keyframesActions.js
@@ -1,79 +1,16 @@
 // TODO: Switch to live fetch once we have a stable API endpoint
-/*
+
 function getKeyframes() {
-  return fetch("/keyframes")
+//  return fetch("/netra.json")
+  return fetch("/data/keyframes.json")
     .then(handleErrors)
     .then(res => res.json());
-}
-*/
-
-// TODO: Temporary to test redux is working
-function fakeGetKeyframes() {
-  return new Promise(resolve => {
-    // Resolve after a timeout so we can see the loading indicator
-    setTimeout(
-      () =>
-        resolve({
-          keyframes: [
-            {
-              id: 0,
-              title: "Keyframe 1",
-              timespan: "07:13-07:21",
-              description: "age 20-29, white, female, standing"
-            },
-            {
-              id: 1,
-              title: "Keyframe 2",
-              timespan: "10:46-10:51",
-              description: "age 20-20, white, female, standing"
-            },
-            {
-              id: 2,
-              title: "Keyframe 3",
-              timespan: "27:13-27:21",
-              description: "age 20-20, white, female, standing"
-            },
-            {
-              id: 3,
-              title: "Keyframe 4",
-              timespan: "30:23-30:21",
-              description: "age 20-20, white, female, standing"
-            },
-            {
-              id: 4,
-              title: "Keyframe 5",
-              timespan: "07:13-07:21",
-              description: "age 20-20, white, female, standing"
-            },
-            {
-              id: 5,
-              title: "Keyframe 6",
-              timespan: "07:13-07:21",
-              description: "age 20-20, white, female, standing"
-            },
-            {
-              id: 6,
-              title: "Keyframe 7",
-              timespan: "07:13-07:21",
-              description: "age 20-20, white, female, standing"
-            },
-            {
-              id: 7,
-              title: "Custom Keyframe 8",
-              timespan: "47:13",
-              description: "-"
-            }
-          ]
-        }),
-      2000
-    );
-  });
 }
 
 export function fetchKeyframes() {
   return dispatch => {
     dispatch(fetchKeyframesBegin());
-    return fakeGetKeyframes()
+    return getKeyframes()
       .then(json => {
         dispatch(fetchKeyframesSuccess(json.keyframes));
         return json.keyframes;
@@ -85,14 +22,13 @@ export function fetchKeyframes() {
 }
 
 // Handle HTTP errors since fetch won't.
-/*
 function handleErrors(response) {
   if (!response.ok) {
     throw Error(response.statusText);
   }
   return response;
 }
-*/
+
 export const FETCH_KEYFRAMES_BEGIN   = 'FETCH_KEYFRAMES_BEGIN';
 export const FETCH_KEYFRAMES_SUCCESS = 'FETCH_KEYFRAMES_SUCCESS';
 export const FETCH_KEYFRAMES_ERROR = 'FETCH_KEYFRAMES_ERROR';
--- src/store/actions/videosActions.js
+++ src/store/actions/videosActions.js
@@ -1,79 +1,15 @@
 // TODO: Switch to live fetch once we have a stable API endpoint
-/*
+
 function getVideos() {
-  return fetch("/videos")
+  return fetch("/data/videos1.json")
     .then(handleErrors)
     .then(res => res.json());
-}
-*/
-// TODO: Temporary to test redux is working
-function fakeGetVideos() {
-  return new Promise(resolve => {
-    // Resolve after a timeout so we can see the loading indicator
-    setTimeout(
-      () =>
-        resolve({
-          videos: [
-            {
-              id: 0,
-              title: "Big Little Lies - Apple iMac",
-              thumbnail: './video.png',
-              uploadedDate: "May 6, 2019",
-              duration: "55:12"
-            },
-            {
-              id: 1,
-              title: "13 Reasons Why - VW",
-              thumbnail: './video.png',
-              uploadedDate: "May 6, 2019",
-              duration: "53:25"
-            },
-            {
-              id: 2,
-              title: "Comedians in Cars - BMW",
-              thumbnail: './video.png',
-              uploadedDate: "May 6, 2019",
-              duration: "43:17"
-            },
-            {
-              id: 3,
-              title: "Stranger Things - Coke",
-              thumbnail: './video.png',
-              uploadedDate: "May 6, 2019",
-              duration: "56:32"
-            },
-            {
-              id: 4,
-              title: "Black Mirror - PGTips",
-              thumbnail: './video.png',
-              uploadedDate: "May 6, 2019",
-              duration: "56:36"
-            },
-            {
-              id: 5,
-              title: "Comedians in Cars - BMW",
-              thumbnail: './video.png',
-              uploadedDate: "May 6, 2019",
-              duration: "47:18"
-            },
-            {
-              id: 6,
-              title: "True Detective - Bud Light",
-              thumbnail: './video.png',
-              uploadedDate: "May 6, 2019",
-              duration: "56:34"
-            }
-          ]
-        }),
-      2000
-    );
-  });
 }
 
 export function fetchVideos() {
   return dispatch => {
     dispatch(fetchVideosBegin());
-    return fakeGetVideos()
+    return getVideos()
       .then(json => {
         dispatch(fetchVideosSuccess(json.videos));
         return json.videos;
@@ -85,14 +21,13 @@ export function fetchVideos() {
 }
 
 // Handle HTTP errors since fetch won't.
-/*
 function handleErrors(response) {
   if (!response.ok) {
     throw Error(response.statusText);
   }
   return response;
 }
-*/
+
 export const FETCH_VIDEOS_BEGIN   = 'FETCH_VIDEOS_BEGIN';
 export const FETCH_VIDEOS_SUCCESS = 'FETCH_VIDEOS_SUCCESS';
 export const FETCH_VIDEOS_ERROR = 'FETCH_VIDEOS_ERROR';
