dbd52e4b4812e880cf3f02fce739807d473a8627
John Dalziel <john@crashposition.com>
2019-09-23 23:08:30 +0100

    Added Brand content data to RHS

--- src/components/editor/keyframesListPanel/KeyframeBrandContentForm.js
+++ src/components/editor/keyframesListPanel/KeyframeBrandContentForm.js
@@ -1,18 +1,54 @@
 import React from "react";
-import { Form } from "semantic-ui-react";
+import { List } from "semantic-ui-react";
 
 function KeyframeBrandContentForm(props) {
+
+/*
+Example data:
+        "brands": [
+            {
+                "brand_category_id": 130,
+                "brand_category_name": "Fitness Apparel",
+                "brand_id": 10491,
+                "confidence": 100,
+                "label": "Nike",
+                "tag_location": [
+                    1024,
+                    493,
+                    156,
+                    162
+                ]
+            },
+            {
+                "brand_category_id": 130,
+                "brand_category_name": "Fitness Apparel",
+                "brand_id": 10491,
+                "confidence": 100,
+                "label": "Nike",
+                "tag_location": [
+                    473,
+                    358,
+                    115,
+                    131
+                ]
+            }
+        ],
+*/
   // Inspect data
-//  const src = props.data;
+  const src = props.data;
 //  const totalItems = src.length;
 
   return (
-    <Form>
-      <Form.Group grouped>
-        <h4>Brand Content List TBD</h4>
-      </Form.Group>
-    </Form>
+    <List>
+      {src.map((item, index) => {
+        return (
+          <List.Item key={index}>
+            <List.Header>{item.brand_category_name}</List.Header>
+            {item.label} ({item.confidence}%)
+          </List.Item>
+        );
+      })}
+    </List>
   );
 }
-
 export default KeyframeBrandContentForm;
