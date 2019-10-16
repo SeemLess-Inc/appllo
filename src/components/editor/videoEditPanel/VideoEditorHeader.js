05b1a8cfe17eecc834121173ddd88fa724a4c8b5
John Dalziel <john@crashposition.com>
2019-10-16 21:14:51 +0100

    Enable and Disable Save button

--- src/components/editor/videoEditPanel/VideoEditorHeader.js
+++ src/components/editor/videoEditPanel/VideoEditorHeader.js
@@ -2,55 +2,64 @@ import React from "react";
 import { connect } from "react-redux";
 import { Header, Grid, Divider, Button } from "semantic-ui-react";
 
-const VideoEditorHeader = props => {
-  // Create an object to render
-  const o = {};
-  o.id = props.currentVideo.id;
-  o.title = props.currentVideo.id;
-  o.uploadedDate = props.currentVideo.hasOwnProperty("uploadedDate")
-    ? props.currentVideo.uploadedDate
-    : "Unknown date";
-  /*
+class VideoEditorHeader extends React.Component {
+  state = {};
+  handleClick() {
+    console.log("TODO: Save Keyframe data");
+  }
+
+  render() {
+    // Create an object to render
+    const o = {};
+    o.id = this.props.currentVideo.id;
+    o.title = this.props.currentVideo.id;
+    o.uploadedDate = this.props.currentVideo.hasOwnProperty("uploadedDate")
+      ? this.props.currentVideo.uploadedDate
+      : "Unknown date";
+    /*
   o.thumbnail = props.currentVideo.hasOwnProperty("thumbnail")
     ? props.currentVideo.thumbnail
     : "./video.png";
   o.state = "Uploaded";
   */
 
-  // Show/Hide video and disable Save button
-  let saveButton;
-  if (o.id === "") {
-    o.title = "Choose a video from the list";
-    o.uploadedDate = "";
-    saveButton = <Button disabled>Save</Button>;
-  } else {
-    saveButton = <Button>Save</Button>;
-  }
+    // Show/Hide video and disable Save button
+    let saveButton;
+    if (o.id === "" || this.props.error === true) {
+      o.title = "Choose a video from the list";
+      o.uploadedDate = "";
+      saveButton = <Button disabled>Save</Button>;
+    } else if (this.props.loading === true) {
+      o.title = "Loading Keyframes";
+      o.uploadedDate = "";
+      saveButton = <Button disabled>Save</Button>;
+    } else {
+      saveButton = <Button onClick={this.handleClick}>Save</Button>;
+    }
 
-  return (
-    <Grid>
-      <Grid.Row>
-        <Grid.Column width={12}>
-          <Header size="medium">
-            {o.title}
-            <Header.Subheader>{o.uploadedDate}</Header.Subheader>
-          </Header>
-        </Grid.Column>
-        <Grid.Column width={4} textAlign="right">
-          {saveButton}
-        </Grid.Column>
-      </Grid.Row>
-      <Divider />
-    </Grid>
-  );
-};
+    return (
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={12}>
+            <Header size="medium">
+              {o.title}
+              <Header.Subheader>{o.uploadedDate}</Header.Subheader>
+            </Header>
+          </Grid.Column>
+          <Grid.Column width={4} textAlign="right">
+            {saveButton}
+          </Grid.Column>
+        </Grid.Row>
+        <Divider />
+      </Grid>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
-  currentVideo: state.currentVideo
+  currentVideo: state.currentVideo,
+  keyframes: state.keyframes.items,
+  loading: state.keyframes.loading,
+  error: state.keyframes.error
 });
-//const mapDispatchToProps = { selectVideo }
-
-export default connect(
-  mapStateToProps
-  //  mapDispatchToProps
-)(VideoEditorHeader);
+export default connect(mapStateToProps)(VideoEditorHeader);
