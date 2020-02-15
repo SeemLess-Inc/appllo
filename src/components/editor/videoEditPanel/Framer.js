6510aae535e37201ebf6be7b202372fe435737eb
keara fallon <kfallonmulcahy@metrik.com>
2020-02-14 23:54:17 -0800

    Framer UI update (JM)

--- src/components/editor/videoEditPanel/Framer.js
+++ src/components/editor/videoEditPanel/Framer.js
@@ -1,24 +1,29 @@
 import React from "react";
-import { Menu, Grid, Divider, Header, Segment } from "semantic-ui-react";
+import { List, Button, Grid, Divider, Header, Segment } from "semantic-ui-react";
 
 const Framer = () => (
   <Grid>
     <Grid.Row>
-      <Grid.Column>
-        <Menu secondary>
+      <Grid.Column width={12}>
           <Header fitted sub header as="h3">
             Framer
           </Header>
-          <Menu.Menu position="right">
-            <Menu.Item>Keyframe 3</Menu.Item>
-            <Menu.Item>33 sec</Menu.Item>
-            <Menu.Item header>33:12 - 34:45</Menu.Item>
-          </Menu.Menu>
-        </Menu>
-        <Segment style={{height: '80px'}}></Segment>
-  
+          <List horizontal>
+            <List.Item>Keyframe 3</List.Item>
+            <List.Item>1min 33sec</List.Item>
+            <List.Item header>33:12 - 34:45</List.Item>
+        </List>  
       </Grid.Column>
+      <Grid.Column width={4}>
+          <Button floated='right' basic compact color='grey'>Save Clip As...</Button>
+      </Grid.Column>
+        
     </Grid.Row>
+    <Grid.Row style={{padding: '0'}}>
+      <Grid.Column>
+      <Segment style={{height: '80px'}}></Segment>
+      </Grid.Column>
+    </Grid.Row>  
   </Grid>
 );
 
