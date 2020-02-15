a5106292d50e74d6226bb946c27684bcf3168fdc
keara fallon <kfallonmulcahy@metrik.com>
2020-02-15 09:11:50 -0800

    button label changes

--- src/components/editor/videoEditPanel/Framer.js
+++ src/components/editor/videoEditPanel/Framer.js
@@ -4,18 +4,19 @@ import { List, Button, Grid, Divider, Header, Segment } from "semantic-ui-react"
 const Framer = () => (
   <Grid>
     <Grid.Row>
-      <Grid.Column width={12}>
+      <Grid.Column width={8}>
           <Header fitted sub header as="h3">
             Framer
           </Header>
-          <List horizontal>
+          <List horizontal floated='left'>
             <List.Item>Keyframe 3</List.Item>
             <List.Item>1min 33sec</List.Item>
             <List.Item header>33:12 - 34:45</List.Item>
-        </List>  
+          </List>  
       </Grid.Column>
-      <Grid.Column width={4}>
-          <Button floated='right' basic compact color='grey'>Save Clip As...</Button>
+      <Grid.Column width={8}>      
+          <Button floated='right' verticalAlign='top' basic compact color='blue'>Save Clip</Button>
+          <Button floated='right' verticalAlign='top' basic compact color='grey'>Create Thumbnail</Button>
       </Grid.Column>
         
     </Grid.Row>
